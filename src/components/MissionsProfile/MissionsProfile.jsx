d916bff34d3badadb2f6e1c29cb9ddad84223587
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-08-30 15:42:23 -0300

    Update package.json / Add button leave Mission in Profile section

--- src/components/MissionsProfile/MissionsProfile.jsx
+++ src/components/MissionsProfile/MissionsProfile.jsx
@@ -1,22 +1,42 @@
 import ListGroup from 'react-bootstrap/ListGroup';
-import { useSelector } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { leaveMission } from '../../redux/missions/missions-slice';
 
 const MissionsProfile = () => {
   const { missions } = useSelector((state) => state.missions);
 
   const filteredMissions = missions.filter((mission) => mission.reserved);
 
+  const dispatch = useDispatch();
+
+  const handleUpdateStore = (id) => {
+    const selectedMission = missions.find((mission) => mission.mission_id === id);
+    if (selectedMission.reserved) {
+      dispatch(leaveMission(id));
+    }
+  };
   return (
     <ListGroup style={{ width: '42%' }}>
       <h2>My Missions</h2>
       {filteredMissions.length > 0 ? (
         filteredMissions.map((mission) => (
-          <ListGroup.Item style={{ padding: '15px' }} key={mission.mission_id}>
+          <ListGroup.Item className="d-flex justify-content-between align-items-center" style={{ padding: '15px', borderRadius: '6px' }} key={mission.mission_id}>
             {mission.mission_name}
+            <Button
+              onClick={() => handleUpdateStore(mission.mission_id)}
+              variant="outline-danger"
+              className="ms-3"
+            >
+              Leave Mission
+            </Button>
           </ListGroup.Item>
         ))
       ) : (
-        <h4 style={{ marginTop: '3rem', color: '#0d6efd', fontSize: '2rem' }}>No missions joined</h4>
+        <ListGroup.Item style={{ borderRadius: '6px' }}>
+          You haven&apos;t joined the missions yet
+
+        </ListGroup.Item>
       )}
     </ListGroup>
   );
